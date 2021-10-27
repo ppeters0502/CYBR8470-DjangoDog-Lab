48ab6dc8bb0b2d7dc41ddf3498f9fe215767a9cf
Pat <pspeters93@gmail.com>
2021-10-26 23:32:35 -0500

    completed lab

--- DogLab/urls.py
+++ DogLab/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from api import controllers
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/breeds', controllers.BreedList.as_view()),
+    path('api/breeds/<int:pk>', controllers.BreedDetail.as_view()),
+    path('api/dogs', controllers.DogList.as_view()),
+    path('api/dogs/<int:pk>', controllers.DogDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
